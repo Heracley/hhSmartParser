4729af35b2973b2c8d1e990e9abcc2637e628462
Heracley <serguzeo@gmail.com>
2023-08-31 21:38:52 +0300

    first commit

---
+++ analyz_by_openai.py
@@ -0,0 +1,76 @@
+import json
+import time
+
+import openai
+import os
+from dotenv import load_dotenv
+#  Загружаем среду и ее переменные.
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+#  Получаем список с тренировочными данными для ChatGPT из выбранного файла. Необходимо указать количество вакансий,
+#  которые используются для тренировки.
+def get_train_for_ai(file_name: str = "trainingdata.txt", count_of_trains: int = 3) -> list:
+    train_data: list = []
+
+    with open(file_name, "r", encoding='utf-8') as f:
+        for i in range(count_of_trains):
+            text = {"role": "user", "content": f.readline()}
+            answer = {"role": "assistant", "content": f.readline()}
+            train_data.extend([text, answer])
+
+    return train_data
+
+
+#
+def get_all_required_competencies(vacancy: str):
+    def ai_find_cmptncs_in_text(text, train_data=None, model="gpt-3.5-turbo-16k"):
+        if train_data is None:
+            train_data = get_train_for_ai()
+
+        sysprompt = """You are part of the task analyzer code with hh.ru . Your main task is to highlight from the 
+        vacancy text the technologies and competencies that an employee needs for this vacancy. Your answer should be all 
+        the technologies and competencies found in the text. The answer should be short - only the technologies 
+        themselves in any convenient order and competencies in lowercase letters, separated by commas, 
+        nothing superfluous. If the technology or competence is composite (consisting of several words), then use "+" as 
+        a separator between words. If you haven't found technologies or competencies, write "None"."""
+
+        messages = [{"role": "system", "content": sysprompt}]
+        messages.extend(train_data)
+        messages.append(
+            {"role": "user", "content": text}
+        )
+        cmptncs = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            temperature=0,
+        ).choices[0].message["content"].replace('\n', '').split(',')
+        return cmptncs
+
+    try:
+        vacancy: dict = json.loads(vacancy)
+    except:
+        return dict()
+
+    all_required_competencies = set()
+    if vacancy["tags"]:
+        for tag in vacancy["tags"]:
+            all_required_competencies.add(tag.lower().replace(' ', '+'))
+
+    cmptncs_from_text = ai_find_cmptncs_in_text(vacancy["text"])
+    for cmptncs in cmptncs_from_text:
+        all_required_competencies.add(cmptncs)
+
+    return all_required_competencies
+
+
+if __name__ == "__main__":
+    with open('data.json', "r", encoding='utf-8') as file:
+        for i in range(50):
+            line = file.readline()[:-3] + "}"
+            all_required_competencies = get_all_required_competencies(line)
+            if all_required_competencies:
+                print(i, all_required_competencies)
+                time.sleep(20)
+
---
+++ parse_hh.py
@@ -0,0 +1,106 @@
+import requests
+from bs4 import BeautifulSoup
+import fake_useragent
+import time
+import json
+
+
+#  Получаем список ссылок на вакансии по заданному запросу (text)
+def get_links(text):
+    #  Создаем fake user-agent для обхода блокировки hh
+    ua = fake_useragent.UserAgent()
+
+    #  Получаем html-страницу, найденную по поиску
+    data = requests.get(
+        url=f"https://hh.ru/search/vacancy?text={text}&area=1&page=0",
+        headers={"user-agent": ua.random}
+    )
+    if data.status_code != 200:
+        return
+    soup = BeautifulSoup(data.content, "lxml")
+
+    #  Считываем из нее количество страниц с вакансиями
+    try:
+        page_count = int(
+            soup.find("div", attrs={"class": "pager"}).find_all("span",
+                                                                recursive=False)[-1].find("a").find("span").text)
+    except:
+        return
+
+    #  Проходимся по всем страницам, выданных по запросу (text) и получаем ссылки на вакансии
+    try:
+        for page in range(page_count):
+            data = requests.get(
+                url=f"https://hh.ru/search/vacancy?text={text}&area=1&page={page}",
+                headers={"user-agent": ua.random}
+            )
+            if data.status_code != 200:
+                continue
+            soup = BeautifulSoup(data.content, "lxml")
+            for link in soup.find_all("a", attrs={"class": "serp-item__title"}):
+                yield link.attrs['href'].split('?')[0]
+    except:
+        return
+    #  Немного спим, чтобы не перезагружать сервера hh и не улететь в бан.
+    time.sleep(1)
+
+
+#  Получаем всю необходимую информацию со страницы вакансии по ссылке (link)
+def get_vacancy(link):
+    #  Создаем fake user-agent для обхода блокировки hh
+    ua = fake_useragent.UserAgent()
+
+    #  Получаем html страницы вакансии
+    data = requests.get(
+        url=link,
+        headers={"user-agent": ua.random}
+    )
+    if data.status_code != 200:
+        return
+    soup = BeautifulSoup(data.content, "lxml")
+
+    #  Получаем "название" вакансии
+    try:
+        name = soup.find("h1", attrs={"class": "bloko-header-section-1"}).text
+    except:
+        name = ""
+    #  Получаем указанную зарплату, если есть.
+    try:
+        salary = soup.find("div", attrs={"data-qa": "vacancy-salary"}).text.replace("\xa0", "")
+    except:
+        salary = ""
+    #  Получаем описание вакансии, если есть.
+    try:
+        text = soup.find("div", attrs={"data-qa": "vacancy-description"}).text
+    except:
+        text = ""
+    #  Получаем ключевые навыки, указанные в вакансии, если есть.
+    try:
+        tags = [x.text for x in soup.find("div", attrs={"class": "bloko-tag-list"}).find_all("span")]
+    except:
+        tags = ""
+
+    #  Создаем словарь(json-объект) вакансии
+    vacancy = {
+        "name": name,
+        "salary": salary,
+        "text": text,
+        "tags": tags,
+        "url": link,
+    }
+    return vacancy
+
+
+if __name__ == "__main__":
+    #  Открываем файл data.json на запись и в удобном для считывания формате записываем все спарсенные вакансии.
+    with open("data.json", "w", encoding="utf-8") as json_file:
+        json_file.write("[\n")
+        for link in get_links("python"):
+            item = get_vacancy(link)
+            #  Немного спим, чтобы не перезагружать сервера hh и не улететь в бан.
+            time.sleep(1)
+            json.dump(item, json_file, separators=(',', ':'), ensure_ascii=False)
+            json_file.write(",\n")
+        json_file.write("]")
+
+
